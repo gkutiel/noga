d285af4c4f1aafb252bcdfca2912558ff12e53e2
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-02-15 13:23:24 +0200

    pytorch

---
+++ noga/model.py
@@ -0,0 +1,36 @@
+import pytorch_lightning as pl
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
+
+
+# 1. Define the System (Model + Training Logic)
+class SimpleModel(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.layer = nn.Linear(1, 1)
+
+    def forward(self, x):
+        return self.layer(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = nn.functional.mse_loss(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.parameters(), lr=0.01)
+
+
+# 2. Prepare Dummy Data (y = 2x)
+x = torch.randn(128, 1)
+y = 2 * x
+dataset = TensorDataset(x, y)
+train_loader = DataLoader(dataset, batch_size=32)
+
+# 3. Train
+model = SimpleModel()
+trainer = pl.Trainer(max_epochs=10, accelerator="cpu")
+trainer.fit(model, train_loader)
