2b566537292a63273aca5fd282fafb8b418a0b45
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-02-15 17:17:12 +0200

    pytorch

--- noga/model.py
+++ noga/model.py
@@ -10,12 +10,26 @@ from torch.utils.data import DataLoader, Dataset
 class Model(pl.LightningModule):
     def __init__(self):
         super().__init__()
+
+        self.month_embedding = nn.Embedding(13, 4)
+        self.day_embedding = nn.Embedding(7, 4)
+        self.hour_embedding = nn.Embedding(24, 4)
+        self.wind_dir_embedding = nn.Embedding(361, 4)
+
         self.net = nn.Sequential(
-            nn.Linear(15, 32),
+            nn.Linear(27, 128),
             nn.LeakyReLU(),
-            nn.Linear(32, 32),
+            nn.Linear(128, 128),
             nn.LeakyReLU(),
-            nn.Linear(32, 32),
+            nn.Linear(128, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 32),
             nn.LeakyReLU(),
         )
 
@@ -36,6 +50,11 @@ class Model(pl.LightningModule):
         ), y = batch
 
         x = torch.cat([
+            self.month_embedding(time[:, 0]),
+            self.day_embedding(time[:, 1]),
+            self.hour_embedding(time[:, 2]),
+            # self.wind_dir_embedding(wind_dir),
+
             temperature,
             humidity,
             wind_speed
@@ -63,7 +82,7 @@ class Data(Dataset):
 
         self.time = torch.tensor(
             data.iloc[:, 1:4].values,
-            dtype=torch.int16)
+            dtype=torch.int32)
 
         self.temperature = torch.tensor(
             data.iloc[:, 4:12].values,
@@ -75,7 +94,7 @@ class Data(Dataset):
 
         self.wind_dir = torch.tensor(
             data.iloc[:, 16:19].values,
-            dtype=torch.int16)
+            dtype=torch.int32)
 
         self.wind_speed = torch.tensor(
             data.iloc[:, 19:22].values,
@@ -85,15 +104,15 @@ class Data(Dataset):
             data.iloc[:, -1].values,
             dtype=torch.float32)
 
-        # NORMALIZE THE DATA
-        self.temperature = (
-            self.temperature - self.temperature.mean(0)) / self.temperature.std(0)
-
-        self.humidity = (
-            self.humidity - self.humidity.mean(0)) / self.humidity.std(0)
-
-        self.wind_speed = (
-            self.wind_speed - self.wind_speed.mean(0)) / self.wind_speed.std(0)
+        assert self.time[:, 0].min() > 0, "Month should be non-negative"
+        assert self.time[:, 0].max() <= 12, f"Month {self.time[:, 0].max()}"
+        assert self.time[:, 1].min() >= 0, "Day should be non-negative"
+        assert self.time[:, 1].max() < 7, "Day should be in the range [0, 6]"
+        assert self.time[:, 2].min() >= 0, "Hour should be non-negative"
+        assert self.time[:, 2].max() < 24, f"Hour {self.time[:, 2].max()}"
+        assert self.wind_dir.min() >= 0, "Wind direction should be non-negative"
+        assert self.wind_dir.max() <= 360, \
+            f"Wind direction should be in the range [1, 360] {self.wind_dir.max()}"
 
     def __len__(self):
         return len(self.y)
